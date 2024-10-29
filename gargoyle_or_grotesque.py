08248ca4a7f60965e451fbb4400fadaee0c27c06
SATOR AREPO TENET OPERA ROTAS <135734473+sator-arepo-tenet-opera-rotas@users.noreply.github.com>
2024-10-29 16:28:11 -0700

    Create gargoyle_or_grotesque.py

---
+++ gargoyle_or_grotesque.py
@@ -0,0 +1,10 @@
+'''
+you may have heard the adage that "gargoyles and garyghouls are *fountains* ... otherwise it's just a sparkling grotesque* 
+
+well what *is* the difference?
+gargoyle: guards the goyles (while they soil)
+grotesque: guards the grotto (while they swallow)
+
+yes yes, this is true! in France, Witches invented Guard-the-Gyoles (gargoyles). 
+
+They basically served as SPEWERS that launched poop out at the perverts who tried to see you pee!
